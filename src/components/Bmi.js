b29cdae8ef89452fef80cc9d0dacb9b5444c7d11
Vlad Spreys <vlad.spreys@bilue.com>
2021-06-09 20:55:03 +1000

    feat: Change the background color

--- src/components/Bmi.js
+++ src/components/Bmi.js
@@ -5,22 +5,27 @@ const Bmi = () => {
   const [bmi, setBmi] = useState("0");
 
   const getBmiClass = bmi => {
-    if (bmi >= 1 && bmi <= 18.5) {
-      return 'Underweight';
-    }
-
+    if (bmi >= 1 && bmi <= 18.5) return 'Underweight';
     if (bmi >= 18.5 && bmi <= 24.9) return 'Normal Weight';
     if (bmi >= 24.9 && bmi <= 29.9) return 'Overweight';
     if (bmi >= 30) return  'Obese';
   }
+  const getBackgroundColor = bmi => {
+    if (bmi >= 1 && bmi <= 18.5) return '#FED88B';
+    if (bmi >= 18.5 && bmi <= 24.9) return '#80ff80';
+    if (bmi >= 24.9 && bmi <= 29.9) return '#FF7F50';
+    if (bmi >= 30) return  '#FF5411';
+  }
 
   const bmiCategory = getBmiClass(bmi)
-
   let bmiClass = (bmi > 0 && bmiCategory) ? bmiCategory.split(' ')[0].toLowerCase() : '';
+  const backgroundColor = getBackgroundColor(bmi);
 
   return (
     <Fragment>
-      <div className="calculator">
+      <div className="calculator" style={{
+        backgroundColor
+      }}>
         <h3> Body Mass Index Calculator</h3>
         <div className="bmi-result-container">
           <div className="bmi-result">
