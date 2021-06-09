f8d484ba04c9d27336d6808962d0e64e1122edcf
Vlad Spreys <vlad.spreys@bilue.com>
2021-06-09 20:44:22 +1000

    feat: Display BMI category

--- src/components/Bmi.js
+++ src/components/Bmi.js
@@ -4,6 +4,18 @@ import BmiCalculator from './BmiCalculator';
 const Bmi = () => {
   const [bmi, setBmi] = useState("0");
 
+  const getBmiClass = bmi => {
+    if (bmi >= 1 && bmi <= 18.5) {
+      return 'Underweight';
+    }
+
+    if (bmi >= 18.5 && bmi <= 24.9) return 'Normal Weight';
+    if (bmi >= 24.9 && bmi <= 29.9) return 'Overweight';
+    if (bmi >= 30) return  'Obese';
+  }
+
+  const bmiCategory = getBmiClass(bmi)
+
   return (
     <Fragment>
       <div className="calculator">
@@ -14,7 +26,7 @@ const Bmi = () => {
               Body Mass Index (BMI) = {bmi}
             </div>
             <div className={`bmi-category`}>
-              Underweight
+              {bmiCategory}
             </div>
           </div>
         </div>
